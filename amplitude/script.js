f89e3112375f41f807be5a48fd061bc22c5d0f8a
Anastasiya Shvedun <nastya.shvedun.03@mail.ru>
2022-10-20 15:17:41 +0300

    add laba#2

--- amplitude/script.js
+++ amplitude/script.js
@@ -1,202 +1,15 @@
 const pi = 3.14159 * 25;
-// 1
-
 let canvas1 = document.getElementById("canvas_1");
 let ctx1 = canvas1.getContext("2d");
-// 2
 let canvas2 = document.getElementById("canvas_2");
 let ctx2 = canvas2.getContext("2d");
-// 3
 let canvas3 = document.getElementById("canvas_3");
 let ctx3 = canvas3.getContext("2d");
-// 3
 let canvas4 = document.getElementById("canvas_4");
 let ctx4 = canvas4.getContext("2d");
-// 5
 let canvas5 = document.getElementById("canvas_5");
 let ctx5 = canvas5.getContext("2d");
-
-//
-butt.onclick = function () {
-    let inp_1 = document.getElementById("f").value;
-    ctx1.strokeStyle = " #AE00B1";
-    ctx1.beginPath();
-    cx = 0;
-    cy = canvas1.height / 2;
-    ctx1.moveTo(cx, cy);
-    for (i = 1; i < 200; i++) {
-        x = i * 2.5;
-        y = inp_1 * Math.sin(10 * i / 180 * Math.PI);
-        ctx1.lineTo(cx + x, cy + y);
-    }
-    ctx1.lineWidth = 2;
-    ctx1.stroke()
-
-    stage_1.onclick = function () {
-        let inp_1 = document.getElementById("f").value;
-
-        ctx2.strokeStyle = "#AE00B1"
-        for (let i = 1; i < canvas2.width; i += 5) {
-            for (var j = 0; j < canvas2.height; j += 3) {
-
-                y = inp_1 * Math.sin(4 * i / 180 * Math.PI);
-                ctx2.strokeRect(i, canvas2.height / 2, 0, y);
-            }
-        }
-
-        stage_2.onclick = function () {
-            let inp_1 = document.getElementById("f").value;
-
-
-            ctx3.strokeStyle = "#AE00B1"
-            for (let i = 1; i < canvas3.width; i += 5) {
-                for (var j = 0; j < canvas3.height; j += 3) {
-
-                    y = inp_1 * Math.sin(4 * i / 180 * Math.PI);
-                    ctx3.strokeRect(i, canvas3.height / 2, 0, y);
-                }
-            }
-
-            stage_3.onclick = function () {
-                let inp_1 = document.getElementById("f").value;
-                let height_4 = canvas4.height - inp_1;
-                let width4 = 45;
-                ctx4.strokeStyle = "#AE00B1"
-                ctx4.lineWidth = 4;
-
-                ctx4.beginPath();
-                ctx4.moveTo(0, canvas4.height);
-                ctx4.lineTo(width4, canvas4.height);
-                ctx4.lineTo(width4, height_4);
-                ctx4.lineTo(width4 * 2, height_4);
-                ctx4.lineTo(width4 * 2, canvas4.height);
-                ctx4.lineTo(width4 * 3, canvas4.height);
-                ctx4.lineTo(width4 * 3, height_4);
-                ctx4.lineTo(width4 * 4, height_4);
-                ctx4.lineTo(width4 * 4, canvas4.height);
-                ctx4.lineTo(width4 * 5, canvas4.height);
-                ctx4.lineTo(width4 * 5, height_4);
-                ctx4.lineTo(width4 * 6, height_4);
-                ctx4.lineTo(width4 * 6, canvas4.height);
-                ctx4.lineTo(width4 * 7, canvas4.height);
-                ctx4.lineTo(width4 * 7, height_4);
-                ctx4.lineTo(width4 * 8, height_4);
-                ctx4.lineTo(width4 * 8, canvas4.height);
-                ctx4.lineTo(width4 * 9, canvas4.height);
-                ctx4.stroke();
-
-                stage_4.onclick = function () {
-                    let inp_1 = document.getElementById("f").value;
-                    let width4 = 45;
-                    ctx5.lineWidth = 2;
-                    ctx5.strokeStyle = " #AE00B1";
-
-                    ctx5.beginPath();
-                    cx = 0;
-                    cy = canvas5.height / 2;
-                    ctx5.moveTo(cx, cy);
-                    for (i = 1; i <= 18; i++) {
-                        x = i * 2.5;
-                        y = inp_1 * Math.sin(30 * i / 180 * Math.PI);
-                        ctx5.lineTo(cx + x, cy + y);
-                    }
-                    ctx5.stroke()
-
-                    ctx5.beginPath();
-                    cx = width4;
-                    cy = canvas5.height / 2;
-                    ctx5.moveTo(cx, cy);
-                    for (i = 1; i < 19; i++) {
-                        x = i * 2.5;
-                        y = inp_1 * 1.8 * Math.sin(-30 * i / 180 * Math.PI);
-                        ctx5.lineTo(cx + x, cy + y);
-                    }
-                    ctx5.stroke()
-
-                    ctx5.beginPath();
-                    cx = width4 * 2;
-                    cy = canvas5.height / 2;
-                    ctx5.moveTo(cx, cy);
-                    for (i = 1; i <= 18; i++) {
-                        x = i * 2.5;
-                        y = inp_1 * Math.sin(30 * i / 180 * Math.PI);
-                        ctx5.lineTo(cx + x, cy + y);
-                    }
-                    ctx5.stroke()
-
-                    ctx5.beginPath();
-                    cx = width4 * 3;
-                    cy = canvas5.height / 2;
-                    ctx5.moveTo(cx, cy);
-                    for (i = 1; i < 19; i++) {
-                        x = i * 2.5;
-                        y = inp_1 * 1.8 * Math.sin(-30 * i / 180 * Math.PI);
-                        ctx5.lineTo(cx + x, cy + y);
-                    }
-                    ctx5.stroke()
-
-                    ctx5.beginPath();
-                    cx = width4 * 4;
-                    cy = canvas5.height / 2;
-                    ctx5.moveTo(cx, cy);
-                    for (i = 1; i <= 18; i++) {
-                        x = i * 2.5;
-                        y = inp_1 * Math.sin(30 * i / 180 * Math.PI);
-                        ctx5.lineTo(cx + x, cy + y);
-                    }
-                    ctx5.stroke()
-
-
-                    ctx5.beginPath();
-                    cx = width4 * 5;
-                    cy = canvas5.height / 2;
-                    ctx5.moveTo(cx, cy);
-                    for (i = 1; i < 19; i++) {
-                        x = i * 2.5;
-                        y = inp_1 * 1.8 * Math.sin(-30 * i / 180 * Math.PI);
-                        ctx5.lineTo(cx + x, cy + y);
-                    }
-                    ctx5.stroke()
-
-                    ctx5.beginPath();
-                    cx = width4 * 6;
-                    cy = canvas5.height / 2;
-                    ctx5.moveTo(cx, cy);
-                    for (i = 1; i <= 18; i++) {
-                        x = i * 2.5;
-                        y = inp_1 * Math.sin(30 * i / 180 * Math.PI);
-                        ctx5.lineTo(cx + x, cy + y);
-                    }
-                    ctx5.stroke()
-
-                    ctx5.beginPath();
-                    cx = width4 * 7;
-                    cy = canvas5.height / 2;
-                    ctx5.moveTo(cx, cy);
-                    for (i = 1; i < 19; i++) {
-                        x = i * 2.5;
-                        y = inp_1 * 1.8 * Math.sin(-30 * i / 180 * Math.PI);
-                        ctx5.lineTo(cx + x, cy + y);
-                    }
-                    ctx5.stroke()
-
-                    ctx5.beginPath();
-                    cx = width4 * 8;
-                    cy = canvas5.height / 2;
-                    ctx5.moveTo(cx, cy);
-                    for (i = 1; i <= 18; i++) {
-                        x = i * 2.5;
-                        y = inp_1 * Math.sin(30 * i / 180 * Math.PI);
-                        ctx5.lineTo(cx + x, cy + y);
-                    }
-                    ctx5.stroke()
-                }
-
-
-            }
-        }
-    }
-}
+let inp_1 = 30;
 // перенос элементов
 var DragManager = new function () {
 
@@ -323,7 +136,145 @@ DragManager.onDragCancel = function (dragObject) {
     dragObject.avatar.rollback();
 };
 
-// DragManager.onDragEnd = function (dragObject, dropElem) {
-//     dragObject.elem.style.display = 'none';
+DragManager.onDragEnd = function (dragObject, dropElem) {
+    key.onclick = function () {
+        key.src = '../amplitude/images/key_closed.svg';
 
-// };
+        ctx1.strokeStyle = " #AE00B1";
+        ctx1.beginPath();
+        cx = 0;
+        cy = canvas1.height / 2;
+        ctx1.moveTo(cx, cy);
+        for (i = 1; i < 200; i++) {
+            x = i * 2.5;
+            y = inp_1 * Math.sin(10 * i / 180 * Math.PI);
+            ctx1.lineTo(cx + x, cy + y);
+        }
+        ctx1.lineWidth = 2;
+        ctx1.stroke()
+
+        stage_1.onclick = function () {
+
+            ctx2.strokeStyle = "#AE00B1";
+
+            for (let i = 1; i < canvas2.width; i += 4) {
+                for (var j = 0; j < canvas2.height; j += 3) {
+                    y = inp_1 * Math.sin(4 * i / 180 * Math.PI);
+                    ctx2.strokeRect(i, canvas2.height / 2, 0, y);
+                }
+            }
+
+            stage_2.onclick = function () {
+
+                ctx3.strokeStyle = "#AE00B1"
+                for (let i = 1; i < canvas3.width; i += 4) {
+                    for (var j = 0; j < canvas3.height; j += 3) {
+                        y = inp_1 * Math.sin(4 * i / 180 * Math.PI);
+                        ctx3.strokeRect(i, canvas3.height / 2, 0, y);
+                    }
+                }
+
+                stage_3.onclick = function () {
+
+                    let height_4 = canvas4.height - inp_1;
+                    let width4 = 45;
+
+
+                    ctx4.beginPath();
+                    ctx4.strokeStyle = "#AE00B1"
+                    ctx4.lineWidth = 4;
+                    ctx4.moveTo(0, canvas4.height);
+                    ctx4.lineTo(width4, canvas4.height);
+                    ctx4.lineTo(width4, height_4);
+                    ctx4.lineTo(width4 * 2, height_4);
+                    ctx4.lineTo(width4 * 2, canvas4.height);
+                    ctx4.lineTo(width4 * 3, canvas4.height);
+                    ctx4.lineTo(width4 * 3, height_4);
+                    ctx4.lineTo(width4 * 4, height_4);
+                    ctx4.lineTo(width4 * 4, canvas4.height);
+                    ctx4.lineTo(width4 * 5, canvas4.height);
+                    ctx4.lineTo(width4 * 5, height_4);
+                    ctx4.lineTo(width4 * 6, height_4);
+                    ctx4.lineTo(width4 * 6, canvas4.height);
+                    ctx4.lineTo(width4 * 7, canvas4.height);
+                    ctx4.lineTo(width4 * 7, height_4);
+                    ctx4.lineTo(width4 * 8, height_4);
+                    ctx4.lineTo(width4 * 8, canvas4.height);
+                    ctx4.lineTo(width4 * 9, canvas4.height);
+                    ctx4.stroke();
+
+                    // stage_4.onclick = function () {
+                    //     let input = document.getElementById("f").value;
+                    //     let width4 = 45;
+                    //     ctx5.lineWidth = 3;
+                    //     ctx5.strokeStyle = " #AE00B1";
+
+                    //     ctx5.stroke()
+                    //     for (let j = 0; j < 200; j += 2) {
+                    //         ctx5.beginPath();
+                    //         cx = 18 * input * j;
+                    //         cy = canvas5.height / 2;
+                    //         ctx5.moveTo(cx, cy);
+                    //         for (i = 1; i <= 18; i++) {
+                    //             x = i * input;
+                    //             y = inp_1 * Math.sin(30 * i / 180 * Math.PI);
+                    //             ctx5.lineTo(cx + x, cy + y);
+                    //         }
+                    //         ctx5.stroke()
+                    //         for (let z = 1; z < 200; z += 2) {
+
+                    //             ctx5.beginPath();
+                    //             cx = 18 * input * z;
+                    //             cy = canvas5.height / 2;
+                    //             ctx5.moveTo(cx, cy);
+                    //             for (i = 1; i <= 18; i++) {
+                    //                 x = i * input;
+                    //                 y = inp_1 * 1.8 * Math.sin(-30 * i / 180 * Math.PI);
+                    //                 ctx5.lineTo(cx + x, cy + y);
+                    //             }
+                    //             ctx5.stroke()
+                    //         }
+                    //     }
+                    // }
+                }
+            }
+
+
+
+        }
+
+    }
+};
+stage_4.onclick = function () {
+    let input = document.getElementById("f").value;
+    let width4 = 45;
+    ctx5.lineWidth = 3;
+    ctx5.strokeStyle = " #AE00B1";
+
+    ctx5.stroke()
+    for (let j = 0; j < 500; j += 2) {
+        ctx5.beginPath();
+        cx = 18 * input * j;
+        cy = canvas5.height / 2;
+        ctx5.moveTo(cx, cy);
+        for (i = 1; i <= 18; i++) {
+            x = i * input;
+            y = inp_1 * Math.sin(30 * i / 180 * Math.PI);
+            ctx5.lineTo(cx + x, cy + y);
+        }
+        ctx5.stroke()
+        for (let z = 1; z < 500; z += 2) {
+
+            ctx5.beginPath();
+            cx = 18 * input * z;
+            cy = canvas5.height / 2;
+            ctx5.moveTo(cx, cy);
+            for (i = 1; i <= 18; i++) {
+                x = i * input;
+                y = inp_1 * 1.8 * Math.sin(-30 * i / 180 * Math.PI);
+                ctx5.lineTo(cx + x, cy + y);
+            }
+            ctx5.stroke()
+        }
+    }
+}
