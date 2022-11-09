a1e2a401bc3f60e10f82f1b57c75aefe25129eae
Anastasiya Shvedun <nastya.shvedun.03@mail.ru>
2022-11-09 16:35:25 +0300

    9.11 /2

--- amplitude/script.js
+++ amplitude/script.js
@@ -14,130 +14,130 @@ let inp_1 = 30;
 
 
 // // перенос элементов 1
-var DragManager = new function () {
+// var DragManager = new function () {
 
-    var dragObject = {};
+//     var dragObject = {};
 
-    var self = this;
+//     var self = this;
 
-    function onMouseDown(e) {
-        if (e.which != 1) return;
-        var elem = e.target.closest('.draggable1');
-        if (!elem) return;
-        dragObject.elem = elem;
-        dragObject.downX = e.pageX;
-        dragObject.downY = e.pageY;
-        return false;
-    }
+//     function onMouseDown(e) {
+//         if (e.which != 1) return;
+//         var elem = e.target.closest('.draggable1');
+//         if (!elem) return;
+//         dragObject.elem = elem;
+//         dragObject.downX = e.pageX;
+//         dragObject.downY = e.pageY;
+//         return false;
+//     }
 
-    function onMouseMove1(e) {
-        if (!dragObject.elem) return;
-        if (!dragObject.avatar) {
-            var moveX = e.pageX - dragObject.downX;
-            var moveY = e.pageY - dragObject.downY;
+//     function onMouseMove1(e) {
+//         if (!dragObject.elem) return;
+//         if (!dragObject.avatar) {
+//             var moveX = e.pageX - dragObject.downX;
+//             var moveY = e.pageY - dragObject.downY;
 
-            if (Math.abs(moveX) < 3 && Math.abs(moveY) < 3) {
-                return;
-            }
-            dragObject.avatar = createAvatar(e);
-            if (!dragObject.avatar1) {
-                dragObject = {};
-                return;
-            }
-            var coords = getCoords(dragObject.avatar);
-            dragObject.shiftX = dragObject.downX - coords.left;
-            dragObject.shiftY = dragObject.downY - coords.top;
+//             if (Math.abs(moveX) < 3 && Math.abs(moveY) < 3) {
+//                 return;
+//             }
+//             dragObject.avatar = createAvatar(e);
+//             if (!dragObject.avatar1) {
+//                 dragObject = {};
+//                 return;
+//             }
+//             var coords = getCoords(dragObject.avatar);
+//             dragObject.shiftX = dragObject.downX - coords.left;
+//             dragObject.shiftY = dragObject.downY - coords.top;
 
-            startDrag(e);
-        }
-        dragObject.avatar.style.left = e.pageX - dragObject.shiftX + 'px';
-        dragObject.avatar.style.top = e.pageY - dragObject.shiftY + 'px';
+//             startDrag(e);
+//         }
+//         dragObject.avatar.style.left = e.pageX - dragObject.shiftX + 'px';
+//         dragObject.avatar.style.top = e.pageY - dragObject.shiftY + 'px';
 
-        return false;
-    }
+//         return false;
+//     }
 
-    function onMouseUp(e) {
-        if (dragObject.avatar) {
-            finishDrag(e);
-        }
-        dragObject = {};
-    }
+//     function onMouseUp(e) {
+//         if (dragObject.avatar) {
+//             finishDrag(e);
+//         }
+//         dragObject = {};
+//     }
 
-    function finishDrag(e) {
-        var dropElem = findDroppable(e);
+//     function finishDrag(e) {
+//         var dropElem = findDroppable(e);
 
-        if (!dropElem) {
-            self.onDragCancel(dragObject);
-        } else {
-            self.onDragEnd(dragObject, dropElem);
-        }
-    }
+//         if (!dropElem) {
+//             self.onDragCancel(dragObject);
+//         } else {
+//             self.onDragEnd(dragObject, dropElem);
+//         }
+//     }
 
-    function createAvatar(e) {
+//     function createAvatar(e) {
 
-        var avatar = dragObject.elem;
-        var old = {
-            parent: avatar.parentNode,
-            nextSibling: avatar.nextSibling,
-            position: avatar.position || '',
-            left: avatar.left || '',
-            top: avatar.top || '',
-            zIndex: avatar.zIndex || ''
-        };
+//         var avatar = dragObject.elem;
+//         var old = {
+//             parent: avatar.parentNode,
+//             nextSibling: avatar.nextSibling,
+//             position: avatar.position || '',
+//             left: avatar.left || '',
+//             top: avatar.top || '',
+//             zIndex: avatar.zIndex || ''
+//         };
 
-        avatar.rollback = function () {
-            old.parent.insertBefore(avatar, old.nextSibling);
-            avatar.style.position = old.position;
-            avatar.style.left = old.left;
-            avatar.style.top = old.top;
-            avatar.style.zIndex = old.zIndex
-        };
+//         avatar.rollback = function () {
+//             old.parent.insertBefore(avatar, old.nextSibling);
+//             avatar.style.position = old.position;
+//             avatar.style.left = old.left;
+//             avatar.style.top = old.top;
+//             avatar.style.zIndex = old.zIndex
+//         };
 
-        return avatar;
-    }
+//         return avatar;
+//     }
 
-    function startDrag(e) {
-        var avatar = dragObject.avatar;
-        document.body.appendChild(avatar);
-        avatar.style.zIndex = 9999;
-        avatar1.style.position = 'absolute';
-    }
+//     function startDrag(e) {
+//         var avatar = dragObject.avatar;
+//         document.body.appendChild(avatar);
+//         avatar.style.zIndex = 9999;
+//         avatar1.style.position = 'absolute';
+//     }
 
-    function findDroppable(event) {
+//     function findDroppable(event) {
 
-        dragObject.avatar.hidden = true;
-        var elem = document.elementFromPoint(event.clientX, event.clientY);
-        dragObject.avatar.hidden = false;
+//         dragObject.avatar.hidden = true;
+//         var elem = document.elementFromPoint(event.clientX, event.clientY);
+//         dragObject.avatar.hidden = false;
 
-        if (elem == null) {
-            return null;
-        }
+//         if (elem == null) {
+//             return null;
+//         }
 
-        return elem.closest('.droppable1');
-    }
+//         return elem.closest('.droppable1');
+//     }
 
-    document.onmousemove = onMouseMove;
-    document.onmouseup = onMouseUp;
-    document.onmousedown = onMouseDown;
+//     document.onmousemove = onMouseMove;
+//     document.onmouseup = onMouseUp;
+//     document.onmousedown = onMouseDown;
 
-    this.onDragEnd = function (dragObject, dropElem) { };
-    this.onDragCancel = function (dragObject) { };
+//     this.onDragEnd = function (dragObject, dropElem) { };
+//     this.onDragCancel = function (dragObject) { };
 
-};
+// };
 
 
-function getCoords(elem) {
-    var box = elem.getBoundingClientRect();
+// function getCoords(elem) {
+//     var box = elem.getBoundingClientRect();
 
-    return {
-        top: box1.top + pageYOffset,
-        left: box1.left + pageXOffset
-    };
+//     return {
+//         top: box1.top + pageYOffset,
+//         left: box1.left + pageXOffset
+//     };
 
-}
-DragManager.onDragCancel = function (dragObject) {
-    dragObject.avatar.rollback();
-};
+// }
+// DragManager.onDragCancel = function (dragObject) {
+//     dragObject.avatar.rollback();
+// };
 
 // // перенос элементов 2
 // var DragManager2 = new function () {
